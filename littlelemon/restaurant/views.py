1078c5632e7338d90b7a3cb40e642b1c777a2d3b
Loc Bui Nhien <15635@student.vgu.edu.vn>
2024-04-05 16:41:40 +0700

    Authentication endpoints added

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
+from .models import Booking, Menu
+
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+# class UserViewSet(viewsets.ModelViewSet):
+#    queryset = User.objects.all()
+#    serializer_class = UserSerializer
+#    permission_classes = [permissions.IsAuthenticated] 
